d3e26dced516836206366f1cd9c06e7e81682a49
chopperkuang <chopperkuang@gmail.com>
2012-11-17 18:07:42 +0800

    删除不需要的文件

--- app/controllers/categories.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose')
-    , Category = mongoose.model('Category');
-
-require('express-mongoose');
-
-exports.index = function(req, res) {
-    res.send(Category.all());
-}
